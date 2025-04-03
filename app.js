4ad9fe517c1c4f65ec9b158dfca4c23872644234
OSM-arch <elmohtadioussama03@gmail.com>
2025-04-03 21:32:45 +0000

    commit to local repo

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 let output = document.getElementById('display');
 
-document.addEventListener('keydown', (event) => {
-    const buttons = document.querySelectorAll('button');
-    for (let button of buttons) {
-        if (event.key == '=') calculate();
-        if (event.key == button.textContent && event.key != '=') display(button.textContent);
-        if (event.key == 'c') clearAll();
-    }
-})
-
 function display(input) {
     output.value += input;
 }
